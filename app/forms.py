6e0223c9c56fbe92cfe5b55cefbb3e810bfc0c24
seb4stian <bastiaanvangool2@gmail.com>
2023-02-14 17:51:19 +0100

    Home page fixed with cool images, everything is responsive. Navbar also fixed the centering.. very weird but ok for now fuck it. Great!

--- app/forms.py
+++ app/forms.py
@@ -3,5 +3,5 @@ from django import forms
 #create your forms here
 
 class GotchiForm(forms.Form):
-    gotchi_id = forms.IntegerField(max_value=25000 ,required=True, widget=forms.TextInput(attrs={'class': "form-control"}))
+    gotchi_id = forms.IntegerField(max_value=25000 ,required=True, widget=forms.TextInput(attrs={'class': "form-control"}), label='')
     # post = forms.IntegerField(max_value=25000 ,required=True, name="user_input")
